8336749ff0153dbd76b785e4d5fa8533fe8aef18
MattYoung-UoY <mjy503@york.ac.uk>
2020-01-28 11:48:06 +0000

    Edited project setup
    Edited project setup

--- desktop/src/com/septagon/game/desktop/DesktopLauncher.java
+++ desktop/src/com/septagon/game/desktop/DesktopLauncher.java
@@ -4,14 +4,9 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.septagon.game.Game;
 
-public class DesktopLauncher
-{
-	public static void main (String[] arg)
-	{
+public class DesktopLauncher {
+	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.width = 640;
-		config.height = 480;
-		config.forceExit = true;
 		new LwjglApplication(new Game(), config);
 	}
 }

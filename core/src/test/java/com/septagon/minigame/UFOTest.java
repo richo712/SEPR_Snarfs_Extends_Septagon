a3f49aa1b31a3c5ddc9b33c3663c9cac1c12fd16
wuchunkin <wuchunkin@hotmail.com>
2020-02-11 12:46:05 +0000

    Add the UFO test

---
+++ core/src/test/java/com/septagon/minigame/UFOTest.java
@@ -0,0 +1,42 @@
+package com.septagon.minigame;
+
+import com.badlogic.gdx.graphics.Texture;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.awt.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UFOTest {
+
+    UFO testufo = null;
+    Texture testure = null;
+
+    @BeforeEach
+    public void setUp() {
+        testufo = new UFO(1, 1, null, 32, 0.5f);
+    }
+
+    @Test
+    public void UFOtest() {
+        assertEquals(testufo.xActual, 1);
+
+    }
+
+    @Test
+    public void isCollidingWith() {
+    }
+
+    @Test
+    public void setxActual() {
+        testufo.setxActual(1f);
+        assertEquals(1f,1f);
+    }
+
+    @Test
+    public void setyActual() {
+        testufo.setyActual(1f);
+        assertEquals(1f,1f);
+    }
+}
